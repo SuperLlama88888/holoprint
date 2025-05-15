95ed0c8c31a6f4c800852b2a6a9008931c228d0c
SuperLlama88888 <116475184+SuperLlama88888@users.noreply.github.com>
2025-05-15 22:16:11 +1000

    Rewrite + upgrade build script
    Now written in more janky JavaScript. Now, embedded CSS/JSON in HTML and embedded HTML in JS will be minified.
    This is probably broken - I need to test in a GitHub Codespace now.

---
+++ ci-cd/removeUnnecessaryFiles.sh
@@ -0,0 +1,14 @@
+rm *.md
+rm assets/*.gif
+rm -r tests
+rm -r supabase
+rm -r data/schemas
+rm -rf .*
+keys=$(jq -r '.inputs | keys[]' "esbuild_meta.json")
+for key in $keys; do
+if [ "$key" != "index.js" ]; then
+	rm "$key"
+fi
+done
+rm esbuild_meta.json
+tree -f
