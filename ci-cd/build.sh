b26df524f8d4ba5aa7b145e01717f4c52bee11f4
SuperLlama88888 <116475184+SuperLlama88888@users.noreply.github.com>
2025-05-16 14:20:57 +1000

    Fix version substitution

--- ci-cd/build.sh
+++ ci-cd/build.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Add production constants
-sed -i 's/const VERSION = "dev";/const VERSION = "${1:-testing}";/g' HoloPrint.js
-sed -i 's/const IN_PRODUCTION = false;/const IN_PRODUCTION = true;/g' index.js
+sed -i "s/const VERSION = 'dev';/const VERSION = '${1:-testing}';/g" HoloPrint.js
+sed -i "s/const IN_PRODUCTION = false;/const IN_PRODUCTION = true;/g" index.js
 
 # Bundle and minify
 cd ci-cd
