cb30a72b999917bb92b2c218c90ff5addb5c5807
SuperLlama88888 <116475184+SuperLlama88888@users.noreply.github.com>
2025-05-16 14:27:46 +1000

    Fix again again again again again again again again again

--- ci-cd/build.sh
+++ ci-cd/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Add production constants
-sed -i "s/const VERSION = "dev";/const VERSION = \"${1:-testing}\";/g" HoloPrint.js
+sed -i "s/const VERSION = \"dev\";/const VERSION = \"${1:-testing}\";/g" HoloPrint.js
 sed -i "s/const IN_PRODUCTION = false;/const IN_PRODUCTION = true;/g" index.js
 
 # Bundle and minify
